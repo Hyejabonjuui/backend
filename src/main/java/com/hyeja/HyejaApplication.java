424a530481a41f172cf4dc188d19f0148e8b5287
hyunil <yhi9839@gmail.com>
2026-09-22 14:48:16 +0900

    chore : 프로젝트 초기 세팅

---
+++ src/main/java/com/hyeja/HyejaApplication.java
@@ -0,0 +1,13 @@
+package com.hyeja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyejaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyejaApplication.class, args);
+	}
+
+}
