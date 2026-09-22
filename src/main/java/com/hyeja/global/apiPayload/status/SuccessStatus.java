ac3d60139d382cb07276b90825f5443d0428185c
hyunil <yhi9839@gmail.com>
2026-09-22 20:19:51 +0900

    feat : [SuccessStatus.java] 추가

---
+++ src/main/java/com/hyeja/global/apiPayload/status/SuccessStatus.java
@@ -0,0 +1,25 @@
+package com.hyeja.global.apiPayload.status;
+
+import com.hyeja.global.apiPayload.code.BaseCode;
+import com.hyeja.global.apiPayload.dto.ReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessStatus implements BaseCode {
+    OK(HttpStatus.OK, "SUCCESS_001", "성공입니다."),
+    CREATED(HttpStatus.CREATED, "SUCCESS_002", "생성되었습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ReasonDTO getReasonDTO() {
+        return ReasonDTO.builder()
+                .httpStatus(httpStatus).isSuccess(true)
+                .code(code).message(message).build();
+    }
+}
